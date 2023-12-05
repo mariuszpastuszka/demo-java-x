285c761086434ed22df1b2b1cc90c37d70741a28
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-12-05 13:22:08 +0100

    [add] - exercise added

---
+++ src/main/java/com/mpas/demo/java21/lang/exercises/package-info.java
@@ -0,0 +1,12 @@
+package com.mpas.demo.java21.lang.exercises;
+
+/**
+ * Exercise 1
+ * Prepare your own sealed vehicle hierarchy:
+ * - Car(Engine, Wheels)
+ * - Rocket(Engine, Hull)
+ * - Boat(Engine, Hull, Propeller)
+ *
+ * Prepare a method to simulate repairing vehicles - fixVehicle. Use swith and data deconstruction.
+ * Handle the cases of missing engine (for all) and missing propeller for boat.
+ */
